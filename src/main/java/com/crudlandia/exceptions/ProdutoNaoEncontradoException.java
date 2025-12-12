8b0dcd813142ea27565a5ce06179fa641dfa665a
leandro-mbs <leandromarques_21@hotmail.com>
2025-12-12 14:20:06 -0300

    Agent: [AGENT] - Criando Exception

---
+++ src/main/java/com/crudlandia/exceptions/ProdutoNaoEncontradoException.java
@@ -0,0 +1,16 @@
+package com.crudlandia.exceptions;
+
+@ApiException("PRODUTO_NAO_ENCONTRADO")
+public class ProdutoNaoEncontradoException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+
+  public ProdutoNaoEncontradoException(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
