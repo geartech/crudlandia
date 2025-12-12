ef94ac6088736e768dd364269f89d6ddcbf72675
leandro-mbs <leandromarques_21@hotmail.com>
2025-12-12 14:22:00 -0300

    Agent: [AGENT] - Criando Repository

---
+++ src/main/java/com/crudlandia/models/repository/ProdutoRepository.java
@@ -0,0 +1,22 @@
+package com.crudlandia.models.repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.crudlandia.models.entities.ProdutoEntity;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoEntity, Long> {
+
+  Optional<ProdutoEntity> findById(Long id);
+
+  boolean existsByCodigo(String codigo);
+
+  boolean existsByNome(String nome);
+}
