45d3277c1e98122a4a1fc7236142c066da1d22f3
leandro-mbs <leandromarques_21@hotmail.com>
2025-12-12 15:02:45 -0300

    Agent: [AGENT] - Criando Service Interface

---
+++ src/main/java/com/crudlandia/services/produto/ProdutoService.java
@@ -0,0 +1,26 @@
+package com.crudlandia.services.produto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import com.crudlandia.dtos.ProdutoDTO;
+import com.crudlandia.exceptions.ProdutoDuplicadoException;
+import com.crudlandia.exceptions.ProdutoNaoEncontradoException;
+
+
+public interface ProdutoService {
+
+  ProdutoDTO criar(String codigo, String nome, BigDecimal valor, String marca, LocalDate validade)
+      throws ProdutoDuplicadoException;
+
+  ProdutoDTO atualizar(
+      Long id, String codigo, String nome, BigDecimal valor, String marca, LocalDate validade)
+      throws ProdutoNaoEncontradoException, ProdutoDuplicadoException;
+
+  ProdutoDTO buscarPorId(Long id) throws ProdutoNaoEncontradoException;
+
+  void deletar(Long id) throws ProdutoNaoEncontradoException;
+
+  List<ProdutoDTO> listar();
+}
