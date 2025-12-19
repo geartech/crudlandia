99a57721c3d1be2ff78ea72203cb63c2fd40a94f
leandro-mbs <leandromarques_21@hotmail.com>
2025-12-19 20:21:42 -0300

    Agent: [AGENT] - Criando Mapper Interface

---
+++ src/main/java/com/crudlandia/mappers/ProdutoMapper.java
@@ -0,0 +1,18 @@
+package com.crudlandia.mappers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.crudlandia.dtos.ProdutoDTO;
+import com.github.pagehelper.Page;
+
+
+@Mapper
+public interface ProdutoMapper {
+
+  Page<ProdutoDTO> listagemProdutos(
+      @Param("codigo") String codigo, @Param("nome") String nome, @Param("ativo") Boolean ativo);
+}
